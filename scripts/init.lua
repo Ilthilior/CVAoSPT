32678ed085e8194d63cf7e9f402969fdd40600c2
Schwartz Gandhi <155924247+SchwartzGandhi@users.noreply.github.com>
2026-06-24 17:54:25 -0400

    make new variant and backend

--- scripts/init.lua
+++ scripts/init.lua
@@ -5,6 +5,18 @@ ENABLE_DEBUG_LOG = true
 local variant = Tracker.ActiveVariantUID
 -- check variant info
 IS_ITEMS_ONLY = variant:find("itemsonly")
+for _, var in ipairs({"vanilla_v", "vanilla_h"}) do
+    if variant:find(var) then
+        IS_VANILLA = true
+        break
+    end
+end
+for _, var in ipairs({"vanilla_v", "archipelago_v"}) do
+    if variant:find(var) then
+        IS_VERTICAL = true
+        break
+    end
+end
 
 print("-- Example Tracker --")
 print("Loaded variant: ", variant)
@@ -34,13 +46,22 @@ if not IS_ITEMS_ONLY then -- <--- use variant info to optimize loading
     -- Maps
     Tracker:AddMaps("maps/maps.jsonc")
     -- Locations
-    Tracker:AddLocations("locations/locations.jsonc")
+    if not IS_VANILLA then
+        Tracker:AddLocations("locations/locations.jsonc")
+    else
+        Tracker:AddLocations("locations/backend_v.jsonc")
+        Tracker:AddLocations("locations/frontend_v.jsonc")
+    end
 end
 
 -- Layout
 Tracker:AddLayouts("layouts/items.jsonc")
-Tracker:AddLayouts("layouts/tracker.jsonc")
 Tracker:AddLayouts("layouts/broadcast.jsonc")
+if IS_VERTICAL then
+    Tracker:AddLayouts("var_vertical/layouts/tracker.jsonc")
+else
+    Tracker:AddLayouts("layouts/tracker.jsonc")
+end
 
 -- AutoTracking for Poptracker
 if PopVersion and PopVersion >= "0.18.0" then
